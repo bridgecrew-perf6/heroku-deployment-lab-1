c890346133909570f3e21f92ec66ca2bfcd50984
ianblakesleet <ianblakesleet@gmail.com>
2022-04-27 18:10:56 -0600

    1

--- server/index.js
+++ server/index.js
@@ -4,9 +4,11 @@ const path = require('path')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '.styles/index.html'))
+  res.sendFile(path.join(__dirname, '../styles/index.html'))
 })
+
 app.use(express.static('styles'))
+
 const port = process.env.PORT || 4005
 
 app.listen(port, () => {
